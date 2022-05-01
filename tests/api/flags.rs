59d9924c32a34de82c59569be0ea23debc56e220
Dan Knutson <dan.knutson@gmail.com>
2022-05-01 12:54:18 -0500

    add lobby key to flags test

--- tests/api/flags.rs
+++ tests/api/flags.rs
@@ -1,14 +1,24 @@
+use std::str::FromStr;
+
 use crate::helpers::*;
 
 use scraper::{Html, Selector};
 use serde::Deserialize;
+use uuid::Uuid;
 
 #[tokio::test]
-async fn flags() {
+async fn flags_smoke() {
     let app = spawn_app().await;
 
     let home_response = app.home_page().await;
-    let _flag_json = parse_flags_from_html(home_response).await;
+    let flag_json = parse_flags_from_html(home_response).await;
+
+    let session_id = flag_json.session.session_id;
+    Uuid::from_str(&session_id).unwrap();
+
+    // NOTE this only tests that the key is present,
+    // but doesn't test the actual structure of a Room json
+    assert_eq!(flag_json.lobby.rooms, vec![]);
 }
 
 async fn parse_flags_from_html(home_response: reqwest::Response) -> ElmFlagsJson {
@@ -27,14 +37,30 @@ async fn parse_flags_from_html(home_response: reqwest::Response) -> ElmFlagsJson
 }
 
 // NOTE this needs to match the elm struct Shared.Flags
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, PartialEq, Eq)]
 #[serde(rename_all = "camelCase")]
 pub struct ElmFlagsJson {
     #[allow(dead_code)] // this is here to test that it can be deserialized
     session: ElmSessionJson,
+    #[allow(dead_code)] // this is here to test that it can be deserialized
+    lobby: LobbyResponse,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
+pub struct LobbyResponse {
+    #[allow(dead_code)] // this is here to test that it can be deserialized
+    rooms: Vec<RoomResponse>,
+}
+
+#[derive(Deserialize, Debug, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
+pub struct RoomResponse {
+    #[allow(dead_code)] // this is here to test that it can be deserialized
+    title: String,
+}
+
+#[derive(Deserialize, Debug, PartialEq, Eq)]
 #[serde(rename_all = "camelCase")]
 pub struct ElmSessionJson {
     #[allow(dead_code)] // this is here to test that it can be deserialized
